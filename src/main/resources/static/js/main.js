2d029d7e0e61a0150d0dc1f4c58b4cd657674789
AntuanRokanten <anton_donntu@mail.ru>
2017-08-19 13:43:13 +0300

    weather js added

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -24,9 +24,6 @@ function subscribeToMotionDetection() {
         $("#motion-message").text("Motion is detected");
         var currentDate = new Date();
 
-        // $("#last-detection-time").text(currentDate.getHours() + ':' + currentDate.getMinutes() + ':' + currentDate.getSeconds());
-        // $("#last-detection-date").text(currentDate.getDate() + '.' + currentDate.getMonth() + '.' + currentDate.getFullYear());
-
         $("#last-detection-time").text(currentDate.toLocaleTimeString());
         $("#last-detection-date").text(currentDate.toLocaleDateString());
 
@@ -48,7 +45,12 @@ function subscribeToTempAndHumUpdates() {
     stompClient.send("/app/temp-and-hum-subscribe");
 
     stompClient.subscribe('/topic/temp-and-hum', function (data) {
-        console.log("Temp: " + data)
+        var json = JSON.parse(data.body);
+
+        var temp = json.temperature.celcius;
+        var hum = json.humidity.value;
+        $("#temp-indoors").text(temp);
+        $("#hum-indoors").text(hum);
     });
 }
 
@@ -58,6 +60,23 @@ $(function () {
         subscribeToTempAndHumUpdates();
     });
 
+    if ("geolocation" in navigator) {
+        console.log("Geolocation is supported by browser. Checking permission for accessing location.");
+        navigator.geolocation.getCurrentPosition(
+        function(position) { // on granted
+            var location = position.coords.latitude+','+position.coords.longitude;
+            console.log("Location permission is granted. Requesting weather for: " + location);
+            loadWeather(location);
+        },
+        function() { // on declined
+            console.log("Location permission is declined.");
+            loadWeatherDefaultLocation();
+        });
+
+    } else {
+        console.log("Browser doesn't support geolocation");
+        loadWeatherDefaultLocation();
+    }
 
     $('#motion-toggle-switch').change(function () {
         var telNotificationSwitch = $('#motion-telegram-notification-switch-label')[0];
@@ -72,9 +91,35 @@ $(function () {
     });
 });
 
+function loadWeatherDefaultLocation() {
+    var defaultCity = 'KHARKIV'; // todo can it be injected with spring?
+    console.log("Requesting weather for default location: " + defaultCity);
+    loadWeather(defaultCity, '');
+}
+
 // function disconnect() {
 //     if (stompClient !== null) {
 //         stompClient.disconnect();
 //     }
 //     console.log("Disconnected");
 // }
+
+function loadWeather(location, woeid) {
+  $.simpleWeather({
+    location: location,
+    woeid: woeid,
+    unit: 'c',
+    success: function(weather) {
+//      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+//      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+//      html += '<li class="currently">'+weather.currently+'</li>';
+//      html += '<li>'+weather.alt.temp+'&deg;C</li></ul>';
+
+      $("#temp-outdoors").text(weather.temp);
+      $("#hum-outdoors").text(weather.humidity);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+}
